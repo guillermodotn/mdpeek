ea4741e898ca38a9814773bc05be5a84b90557dd
guillermodotn <guillerm0.n@outlook.es>
2026-06-03 16:04:30 +0200

    chore: bump version to 0.3.0 and derive it from CMake in Qt main

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("mdpeek"));
-    app.setApplicationVersion(QStringLiteral("0.1.0"));
+    app.setApplicationVersion(QStringLiteral(MDPEEK_VERSION));
 
     MdpeekViewer viewer(fi.absoluteFilePath());
     viewer.show();
